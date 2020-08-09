e7636f5fe038b41419c290215a4414ace1bac345
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-08-09 18:15:19 +0530

    Commit

--- js/animations/home-anim.js
+++ js/animations/home-anim.js
@@ -3,5 +3,7 @@ var tl = gsap.timeline();
 tl.from('nav', 0.7, {y: 100, opacity: 0});
 tl.staggerFrom('.link-holder ul li', 0.7, {y: 100, opacity: 0}, 0.2);
 tl.from('.toggle-switch', 0.7, {y: 100, opacity: 0});
-tl.from('.hero', 1, {y: 100, opacity: 0});
+tl.from('.intro-text', 1, {y: 100, opacity: 0});
+tl.from('.gradient-border', 1, {height: 0, opacity: 0});
+tl.from('.portfolio-div', 0.7, {y: 0, opacity: 0});
 tl.staggerFrom('.social-icon', 1, {y: 100, opacity: 0}, 0.2);
