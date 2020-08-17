7417ea9e079112c88c1df0879017b7e1aa227c93
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-08-17 16:22:07 +0530

    commit

--- js/animations/home-anim.js
+++ js/animations/home-anim.js
@@ -1,6 +1,7 @@
 var tl = gsap.timeline();
 
 tl.from('nav', 0.7, {y: 100, opacity: 0});
+tl.from('.hamburger', 1, {y: 100, opacity: 0})
 tl.staggerFrom('.link-holder ul li', 0.7, {y: 100, opacity: 0}, 0.2);
 tl.from('.toggle-switch', 0.7, {y: 100, opacity: 0});
 tl.from('.intro-text', 1, {y: 100, opacity: 0});
