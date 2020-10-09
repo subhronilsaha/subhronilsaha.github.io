788e558f4a4c8239aa612d13267709c126278528
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-10-09 17:38:59 +0530

    fixed mobile nav

--- js/animations/home-anim.js
+++ js/animations/home-anim.js
@@ -1,10 +1,12 @@
 var tl = gsap.timeline();
 
-tl.from('nav', 0.7, {y: 100, opacity: 0});
-tl.from('.hamburger', 1, {y: 100, opacity: 0})
-tl.staggerFrom('.link-holder ul li', 0.7, {y: 100, opacity: 0}, 0.2);
-tl.from('.toggle-switch', 0.7, {y: 100, opacity: 0});
+tl.from('nav', 1, {y: 100, opacity: 0});
+
+if(screen.width <= 768) {
+    tl.from('.hamburger', 1, {y: 100, opacity: 0})
+} else {
+    tl.staggerFrom('.link-holder ul li', 0.7, {y: 100, opacity: 0}, 0.2);
+}
+
 tl.from('.intro-text', 1, {y: 100, opacity: 0});
-tl.from('.gradient-border', 1, {height: 0, opacity: 0});
-tl.from('.portfolio-div', 0.7, {y: 0, opacity: 0});
 tl.staggerFrom('.social-icon', 1, {y: 100, opacity: 0}, 0.2);
