882fce810ed33ef8829979833507027477e4f82e
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-10-20 11:24:28 +0530

    updated about section

--- js/animations/home-anim.js
+++ js/animations/home-anim.js
@@ -1,4 +1,5 @@
 var tl = gsap.timeline();
+// var tween = gsap.tweenMax();
 
 tl.from('nav', 1, {y: 100, opacity: 0});
 
@@ -11,7 +12,8 @@ if(screen.width <= 768) {
 tl.from('.intro-text', 1, {y: 100, opacity: 0});
 tl.staggerFrom('.social-icon', 1, {y: 100, opacity: 0}, 0.2);
 
-tl.from('#about', 1, {y: 100, opacity: 0});
+tl.from('.about', 1, {y: 100, opacity: 0});
 
 tl.from('#projects', 1, {y: 100, opacity: 0});
+
 
